601e0faf361f5707a58b7fbec74e2ebe48ab1267
Onur Genes <onur@onurgenes.com>
2019-05-15 11:47:38 +0300

    Initial Commit

---
+++ FoursquarePOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquarePOC
+//
+//  Created by Onur Geneş on 15.05.2019.
+//  Copyright © 2019 Onur Geneş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
